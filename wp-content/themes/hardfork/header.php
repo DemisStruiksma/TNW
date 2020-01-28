cd13595385245302da9b17b62ea4a8cfca511bdb
Demis Struiksma <contact@demis.io>
2020-01-28 14:45:17 +0100

    Added Bootstrap

---
+++ wp-content/themes/hardfork/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php wp_footer();?>
+
+    <!-- <meta charset="utf-8">
+    <title></title> -->
+  </head>
+  <body <?php body_class();?>>
