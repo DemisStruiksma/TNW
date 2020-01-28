a092740d9f216232088cbfa383c480adb1bb99a1
Demis Struiksma <contact@demis.io>
2020-01-28 18:25:35 +0100

    Added TNW style assets

--- wp-content/themes/hardfork/functions.php
+++ wp-content/themes/hardfork/functions.php
@@ -7,10 +7,37 @@ function load_stylesheets()
     false, 'all');
   wp_enqueue_style('bootstrap');
 
+  wp_register_style('tnw-genesis', get_template_directory_uri() . '/css/tnw-genesis.css', array(), false, 'all');
+  wp_enqueue_style('tnw-genesis');
+
+  wp_register_style('tnw-style', get_template_directory_uri() . '/css/tnw-style.css', array(), false, 'all');
+  wp_enqueue_style('tnw-style');
+
   wp_register_style('style', get_template_directory_uri() . '/style.css', array(), false, 'all');
   wp_enqueue_style('style');
 
-
-
 }
 add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+
+// function include_jquery()
+// {
+//
+//   wp_deregister_script('jquery');
+//
+//   wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery-3.4.1.min.js', '', 1, true);
+//
+//   add_action('wp_enqueue_scripts', 'jquery');
+// }
+// add_action('wp_enqueue_scripts', 'include_jquery');
+//
+//
+//
+// function loadjs()
+// {
+//
+//   wp_register_script('customjs', get_template_directory_uri() . '/js/scripts.js', '', 1, true);
+//   wp_enqueue_script('customjs');
+//
+// }
+// add_action('wp_enqueue_scripts', 'loadjs');
