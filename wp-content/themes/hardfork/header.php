37fa501eaec33a8cfa2a837d5beb9e2b79ed530e
Demis Struiksma <contact@demis.io>
2020-02-02 17:57:50 +0100

    Fixed posts

--- wp-content/themes/hardfork/header.php
+++ wp-content/themes/hardfork/header.php
@@ -16,94 +16,14 @@
     <meta name="description" content="News, announcement and insider stories for crypto traders">
     <meta property="og:locale" content="en_US">
 
-    <link rel="dns-prefetch" href="//cdn0.tnwcdn.com/">
-    <link rel="dns-prefetch" href="//cdn4.tnwcdn.com/">
-    <link rel="dns-prefetch" href="//www.googletagmanager.com">
-    <link rel="preconnect" href="//cdn0.tnwcdn.com/">
-    <link rel="preconnect" href="//cdn4.tnwcdn.com/">
-    <link rel="preconnect" href="//www.googletagmanager.com">
-    <link rel="manifest" href="/manifest.json">
-
-    <!-- Icons -->
-    <link rel="apple-touch-icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/apple-touch-icon-57x57.png?v=1510743095" sizes="57x57">
-    <link rel="apple-touch-icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/apple-touch-icon-60x60.png?v=1510743095" sizes="60x60">
-    <link rel="apple-touch-icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/apple-touch-icon-72x72.png?v=1510743095" sizes="72x72">
-    <link rel="apple-touch-icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/apple-touch-icon-76x76.png?v=1510743095" sizes="76x76">
-    <link rel="apple-touch-icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/apple-touch-icon-114x114.png?v=1510743095" sizes="114x114">
-    <link rel="apple-touch-icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/apple-touch-icon-120x120.png?v=1510743095" sizes="120x120">
-    <link rel="apple-touch-icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/apple-touch-icon-144x144.png?v=1510743095" sizes="144x144">
-    <link rel="apple-touch-icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/apple-touch-icon-152x152.png?v=1510743095" sizes="152x152">
-    <link rel="apple-touch-icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/apple-touch-icon-180x180.png?v=1510743095" sizes="180x180">
-    <link rel="icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/favicon-16x16.png?v=1510743095" sizes="16x16" type="image/png">
-    <link rel="icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/favicon-32x32.png?v=1510743095" sizes="32x32" type="image/png">
-    <link rel="icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/favicon-96x96.png?v=1510743095" sizes="96x96" type="image/png">
-    <link rel="icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/favicon-194x194.png?v=1510743095" sizes="194x194" type="image/png">
-    <link rel="icon" ref="https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/android-chrome-192x192.png?v=1510743095" sizes="192x192" type="image/png">
-    <link rel="shortcut icon" ref="/sub-brand/hardfork.ico">
-    <meta name="apple-mobile-web-app-title" content="Hard Fork">
-    <meta name="application-name" content="Hard Fork">
-    <meta name="theme-color" content="#660099">
-    <link rel="alternate" type="application/rss+xml" title="The Next Web RSS Feed" href="http://feeds2.feedburner.com/thenextweb">
-
-
-    <!-- BEGIN TAGS -->
-    <!-- Social Provider -->
-    <link href="" rel="publisher">
-    <meta name="yandex-verification" content="66df89eec58f9170">
-    <meta property="fb:app_id" content="378011798897423">
-        <meta property="fb:pages" content="106976563522">
-        <meta property="fb:pages" content="136548879865026">
-        <meta property="fb:pages" content="252857631761065">
-        <meta property="fb:pages" content="665773266831844">
-        <meta name="twitter:account_id" content="10876852">
-
-
-
-        <!-- Tag Realm: Home -->
-    <link rel="canonical" href="https://thenextweb.com/hardfork/">
-    <meta property="og:type" content="object">
-    <meta property="og:title" content="Hard Fork">
-    <meta property="og:description" content="News, announcement and insider stories for crypto traders">
-    <meta property="og:url" content="https://thenextweb.com/hardfork/?utm_campaign=OGshare">
-    <meta property="og:site_name" content="Hard Fork | The Next Web">
-    <meta property="og:image" content="https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/04/hardfork-feature-image.jpg">
-    <meta property="og:image:secure_url" content="https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/04/hardfork-feature-image.jpg">
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:description" content="News, announcement and insider stories for crypto traders">
-    <meta name="twitter:title" content="Hard Fork | The Next Web">
-    <meta name="twitter:site" content="@thenextweb">
-    <meta name="twitter:image" content="https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/04/hardfork-feature-image.jpg">
-    <script type="application/ld+json">{"@context":"http:\/\/schema.org","@type":"WebSite","@id":"#website","url":"https:\/\/thenextweb.com\/","name":"TNW","potentialAction":{"@type":"SearchAction","target":"https:\/\/thenextweb.com\/?q={search_term_string}","query-input":"required name=search_term_string"}}</script>
-    <script type="application/ld+json">{"@context":"http:\/\/schema.org","@type":"Organization","url":"https:\/\/thenextweb.com\/hardfork\/","sameAs":["https:\/\/www.facebook.com\/thenextweb","https:\/\/instagram.com\/thenextweb\/","https:\/\/www.linkedin.com\/company\/the-next-web","https:\/\/plus.google.com\/+thenextweb","https:\/\/www.youtube.com\/user\/thenextweb","https:\/\/www.pinterest.com\/thenextweb\/","https:\/\/twitter.com\/thenextweb"],"@id":"#organization","name":"TNW","logo":"https:\/\/cdn1.tnwcdn.com\/wp-content\/blogs.dir\/1\/files\/2016\/06\/tnw-logo-amp.png"}</script>
-
-    <script async src="https://cdn.permutive.com/970c370e-66b5-4cc7-ad17-1d370e11bf75-web.js"></script>
-
-        <!-- END TAGS -->
-
     <script src="//cdn0.tnwcdn.com/sub-brand/assets/js/lib/modernizr-custom.js"></script>
     <link href="//cdn0.tnwcdn.com/sub-brand//css/genesis.css?id=de3da8b7a2e0b3c6d7b4" rel="stylesheet" type="text/css" >
     <link href="//cdn0.tnwcdn.com/sub-brand//css/style.css?id=bafb59ca26dd0c8d8825" rel="stylesheet" type="text/css" >
 
-    <!-- DataLayer -->
-    <script type="text/javascript">
-        var googletag = googletag || {};
-        googletag.cmd = googletag.cmd || [];
-        window.dataLayer = window.dataLayer || [];
-    </script>
-    <!-- TODO -->
-
-
-        <script type="text/javascript">var msTag = {"site":"tnw","page":"category","data":{"category":"hardfork","isSponsoredCategory":"Yes","sponsorName":"null"}};</script>
-                <script src="//massariuscdn.com/pubs/tnw/tnw_adtech_setup.js"></script>
-
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MHDDSC');</script>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MHDDSC" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
   </head>
   <body <?php body_class();?>>
-    <nav class="c-nav" id="nav">
+    <nav class="c-nav fixed-top" id="nav">
 <div class="c-nav-mobile">
 
     <div class="c-nav-mobile-wrapper">
@@ -145,8 +65,8 @@
         </div>
     </div>
 </div>
-<a class="c-nav-logo" href="/">
-    <svg><use xlink:href="/sub-brand/assets/img/icons.svg#tnw"></use></svg>
+<a class="c-nav-logo" href="#">
+    <svg><use xlink:href=""></use></svg>
 </a>
 <a class="c-nav-logoChannel" href="/hardfork">
     <svg preserveAspectRatio="xMinYMin meet" viewBox="0 0 116 24" xmlns="http://www.w3.org/2000/svg">
@@ -200,28 +120,6 @@
 
 
     <ul class="c-nav-icons">
-        <li class="cookieConsent">
-            <a href="#"><svg><use xlink:href="/sub-brand/assets/img/icons.svg#cookie"></use></svg></a>
-            <div class="cookieConsent-popOut">
-                <header>
-                    <h2>Have a cookie</h2>
-                </header>
-                <main>
-                <p>
-                TNW uses cookies to personalize content and ads to
-                make our site easier for you to use.
-                We do also share that information with third parties for
-                advertising &amp; analytics.
-                </p>
-                </main>
-                <footer>
-                    <button class="button button--small" onclick="TNWCookieConsent.accept();"  data-event-category="Top" data-event-action="Cookies" data-event-label="OK" data-event-non-interaction="true">Got it!</button>
-                    <span>
-                        or <a href="/privacy-policy/"  data-event-category="Top" data-event-action="Cookies" data-event-label="More information" data-event-non-interaction="true">More info</a>
-                    </span>
-                </footer>
-            </div>
-        </li>
         <li class="c-nav-icons-toggleMenu">
             <a class="c-nav-toggleMenu" href="#" id="navToggleMenu">
                 <div></div>
@@ -241,31 +139,5 @@
 </a>
 
 </nav>
-    <div class="app
-            app--withPromo
-    ">
-        <div class="app-main">
-                <div class="ad ad--billboard ad--channelTop --hardfork-ads">
-    <div class="tnw-ad ad-unit-TNW_ROS_HEADER" id="TNW_ROS_HEADER" data-ad-block-id="TNW_ROS_HEADER"></div>
-</div>
-<header class="p-channel-header">
-    <div class="js-tnwParallax p-channel-header-visual" data-parallax>
-        <div class="p-channel-header-visual-image js-tnwParallax-content" style="height: 133.33333%;"></div>
-    </div>
-    <div class="u-wrapper">
-                    <a class="p-channel-header-logo" href="/hardfork">
-            <svg preserveAspectRatio="xMinYMin meet" viewBox="0 0 116 24" xmlns="http://www.w3.org/2000/svg">
-<path d="M8.886 4.24h4.225v15.541H8.886v-5.669H4.202v5.67H0V4.24h4.202v6.194h4.684V4.241zm14.096 0l5.625 15.541h-4.334l-.788-2.517h-4.99l-.788 2.517H13.35l5.647-15.54h3.984zm-.57 9.544l-1.4-4.487h-.044l-1.4 4.487h2.845zm15.672.613l3.523 5.384H36.77l-2.933-4.75h-.963v4.75h-4.027V4.241h6.216c3.918 0 5.669 2.32 5.669 5.516 0 2.276-.985 3.83-2.648 4.618v.022zM36.42 9.669c0-.963-.766-1.773-2.276-1.773h-1.27v3.546h1.27c1.313 0 2.276-.613 2.276-1.773zm19.24 2.364c0 4.574-2.956 7.748-7.99 7.748h-5.669V4.241h5.604c4.902 0 8.054 3.195 8.054 7.792zm-4.356 0c0-2.32-1.576-4.093-3.612-4.093h-1.664v8.142h1.358c2.363 0 3.918-1.816 3.918-4.049zm13.46-4.093h6.654v-3.7H60.736v15.541h4.028v-5.23h5.45v-3.678h-5.45V7.94zm22.74 4.071c0 4.75-3.348 7.989-8.076 7.989-4.771 0-8.076-3.261-8.076-7.989S74.657 4 79.428 4c4.728 0 8.077 3.24 8.077 8.011zm-4.355 0c0-2.276-1.51-4.115-3.72-4.115-2.19 0-3.744 1.817-3.744 4.115s1.555 4.115 3.743 4.115c2.21 0 3.721-1.817 3.721-4.115zm14.512 2.386l3.523 5.384h-4.837l-2.933-4.75h-.963v4.75h-4.027V4.241h6.216c3.918 0 5.669 2.32 5.669 5.516 0 2.276-.985 3.83-2.648 4.618v.022zm-1.664-4.728c0-.963-.766-1.773-2.276-1.773h-1.27v3.546h1.27c1.313 0 2.276-.613 2.276-1.773zm14.181-5.428a.547.547 0 0 0-.513.357l-1.99 5.395a.274.274 0 0 1-.257.18h-1.703V4.24h-4.137v15.54h4.137V13.85h3.7c.23 0 .435-.143.514-.358l1.99-5.395a.273.273 0 0 1 .257-.18h3.416V4.242h-5.414zm.643 15.54h4.771v-3.677h-4.771v3.677z" fill="#FFF" />
-</svg>
-        </a>
-                    <div class="p-channel-banner-partner">
-    <p>Powered by</p>
-    <a class="p-channel-banner-partner-link" data-event-category="Channel" data-event-action= "hardfork" data-event-label="Header Logo  - hardfork " href="https://www.etoro.com/" target="_blank"><img alt="eToro" class="p-channel-banner-partner-logo" src="/sub-brand/assets/channels/hardfork/partner.svg"></a>
-</div>
-    </div>
-    <div class="u-wrapper">
-                        <p class="p-channel-header-tagline  ">
-                Blockchain, cryptocurrencies, and insider stories by TNW.
-            </p>
-                </div>
+
               </header>
